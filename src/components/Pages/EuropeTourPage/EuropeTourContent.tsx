20b44424d7dab4b1388b8260b9a8b56e00739833
dev <tech.eng.me@gmail.com>
2025-03-26 08:34:06 -0700

    feat: tour page

--- src/components/Pages/EuropeTourPage/EuropeTourContent.tsx
+++ src/components/Pages/EuropeTourPage/EuropeTourContent.tsx
@@ -1,4 +1,5 @@
 import RecBar from "../../RecBar"
+import list from "./tourlist.json"
 
 const EuropeTourContent = ({ isPopup = false }) => (
   <div className="p-[5px] md:p-[10px] border-[2px] border-gray_1 h-full">
@@ -13,7 +14,28 @@ const EuropeTourContent = ({ isPopup = false }) => (
         px-[15px] md:px-[20px] flex flex-col gap-y-[5px] ${
           isPopup ? "md:gap-y-[10px]" : "md:gap-y-[20px]"
         }`}
-      ></div>
+      >
+        {list.map((item, i) => (
+          <div key={i} className="flex justify-between">
+            <div className="text-sm w-fit">
+              <p className="text-left">{new Date(item.startDate).toLocaleString()}</p>
+              <p className="text-left">{item.location.name}</p>
+              <p className="text-left">{`${item.location.address.addressLocality}, ${item.location.address.addressCountry}`}</p>
+            </div>
+            <div className="flex gap-2">
+              <button
+                className="border text-sm"
+                onClick={() => window.open(item.offers[0].url, "_blank")}
+              >
+                Tickets
+              </button>
+              <button className="border text-sm" onClick={() => window.open(item.url, "_blank")}>
+                RSVP
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
       <RecBar cctvNumber={3} />
     </div>
   </div>
